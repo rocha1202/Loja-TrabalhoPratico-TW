d85a912e95817dc14cdfb51a797af608dee7a42d
rocha1202 <40220247@esmad.ipp.pt>
2023-01-14 11:39:33 +0000

    criação da nova pagina inicial

---
+++ js/carrossel.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 100, /* TEMPO PARA TROCAR AS IMAGENS COM O RATO SOBRE AS IMAGENS*/
+    slidesToShow: 1, /* IMAGENS POR CARROSSEL */
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000 /* TEMPO PARA TROCAR AS IMAGENS COM O RATO SEM ESTAR EM CIMA DAS IMAGENS*/
+});
