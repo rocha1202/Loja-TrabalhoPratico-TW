bb5b831953b025761f674ba70587d8260d21249b
rocha1202 <40220247@esmad.ipp.pt>
2023-01-19 23:41:22 +0000

    DropDown index

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
+let texto = sessionStorage.getItem('login')
+console.log(texto)
+const sair = () => {
+    localStorage.removeItem('login')
+    sessionStorage.removeItem('login')
+
+    window.location.href = ''
+  }
+if (texto!=null){
 $(document).ready(() => {
-    let texto = sessionStorage.getItem('login')
+   
     $('#user').css({
         width: '100%',
         marginLeft: '10%'
     })
     $('#user').html(texto)
     $('#login').html("")
-})
+})}
