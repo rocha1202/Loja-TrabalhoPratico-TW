c37e056a70cd86ec40f6eb0254dda793edbb3c13
rocha1202 <40220247@esmad.ipp.pt>
2023-01-19 21:16:36 +0000

    login

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+    let texto = sessionStorage.getItem('login')
+    $('#user').css({
+      width: '100%',
+      marginLeft: '10%'
+    })
+    $('#user').html(texto)
+    $('#login').html("")
+  })
