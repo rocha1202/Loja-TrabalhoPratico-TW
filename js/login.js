a2bef04241317666a174aee8a6298efbedcd5280
Filinto Delgado <filintodelgado@gmail.com>
2023-01-20 13:25:53 +0000

    adcionado transição aos sponsors

--- js/login.js
+++ js/login.js
@@ -7,14 +7,14 @@ const password = document.querySelector("#password");
 const remenber = document.querySelector("#remenber");
 
 form.addEventListener("submit", (event) => {
-event.preventDefault();
- /** O nome do usuario se o login succeder ou false caso contrario */
- const success = login(username.value, password.value, remenber.checked)
+  event.preventDefault();
+  /** O nome do usuario se o login succeder ou false caso contrario */
+  const success = login(username.value, password.value, remenber.checked)
 
- if(!success) {
-   div.className = "error"
-   return
- }
+  if(!success) {
+    div.className = "error"
+    return
+  }
 
- home()
+  home()
 })
